88763728c1ba8246550b22c339db85219b912cff
Sergio Orejarena <sergioorueda@gmail.com>
2021-10-19 18:55:09 -0500

    Code cleaning

---
+++ functions/src/analize.ts
@@ -0,0 +1,60 @@
+import * as admin from "firebase-admin";
+import * as AWS from "aws-sdk";
+import {toParse} from "./parser";
+
+
+AWS.config.loadFromPath("./config.json");
+const Comprehend = new AWS.Comprehend();
+
+
+/**
+ * This function is in charge of performing all the necessary
+ * analysis within the chat to return your insights.
+ *
+ * @param {string} text - Text to parse.
+ *
+ * @return {Promise<Record<string, unknown>>} - Returns the insights of
+ * the entire chat as well as those of each participant.
+ */
+export async function analyze(text: string): Promise<Record<string, unknown>> {
+  try {
+    const responses: Record<string, unknown> = {};
+    const data = toParse(text);
+
+    for (const author of data) {
+      const response: Record<string, unknown> = {};
+      const params = {
+        Text: author.messages,
+        LanguageCode: "",
+      };
+
+      const LanguageResponse = await Comprehend.detectDominantLanguage(
+          {Text: text}).promise();
+      params.LanguageCode = LanguageResponse.Languages![0].LanguageCode!;
+
+      const EntitiesResponse = await Comprehend.detectEntities(
+          params).promise();
+      const Entities = EntitiesResponse.Entities;
+      for (const entity of Entities!) {
+        delete entity["Score"];
+        delete entity["BeginOffset"];
+        delete entity["EndOffset"];
+      }
+      response.Entities = Entities;
+
+      const SentimentsResponse = await Comprehend.detectSentiment(
+          params).promise();
+      response.Sentiments = SentimentsResponse;
+
+      response.Text = [author.messages];
+      responses[author.author] = response;
+    }
+
+    responses.queryTime = admin.firestore.FieldValue.serverTimestamp();
+    responses.fullChat = text;
+
+    return responses;
+  } catch (err) {
+    return {Error: err};
+  }
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,63 +1,28 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as express from "express";
-import * as AWS from "aws-sdk";
 import * as cors from "cors";
-import {toParse} from "./parser";
+import {analyze} from "./analize";
 
 
 admin.initializeApp(functions.config().firebase);
-AWS.config.loadFromPath("./config.json");
 
 const app = express();
 app.use(cors({origin: true}));
 
-const Comprehend = new AWS.Comprehend();
 const db = admin.firestore();
 
 
 app.post("/", async (req, res) => {
-  try {
-    const responses: Record<string, unknown> = {};
-    const data = toParse(req.body.Text);
+  const response = await analyze(req.body.Text);
 
-    for (const author of data) {
-      const response: Record<string, unknown> = {};
-      const params = {
-        Text: author.messages,
-        LanguageCode: "",
-      };
-
-      const LanguageResponse = await Comprehend.detectDominantLanguage(
-          req.body).promise();
-      params.LanguageCode = LanguageResponse.Languages![0].LanguageCode!;
-
-      const EntitiesResponse = await Comprehend.detectEntities(
-          params).promise();
-      const Entities = EntitiesResponse.Entities;
-      for (const entity of Entities!) {
-        delete entity["Score"];
-        delete entity["BeginOffset"];
-        delete entity["EndOffset"];
-      }
-      response.Entities = Entities;
-
-      const SentimentsResponse = await Comprehend.detectSentiment(
-          params).promise();
-      response.Sentiments = SentimentsResponse;
-
-      response.Text = [author.messages];
-      responses[author.author] = response;
-    }
-
-    responses.queryTime = admin.firestore.FieldValue.serverTimestamp();
-    responses.fullChat = req.body.Text;
-
-    res.send(responses);
-    db.collection("History").add(responses);
-  } catch (err) {
-    res.send(err);
+  if (!("Error" in response)) {
+    db.collection("History").add(response);
   }
+
+  console.log(response);
+
+  res.send(response);
 });
 
 
